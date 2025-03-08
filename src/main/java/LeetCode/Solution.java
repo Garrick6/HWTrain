07c2987028384ff1751f4fb9076af92647fbf9cf
李国龙 <2670775238@qq.com>
2025-03-08 17:19:02 +0800

    保存

---
+++ src/main/java/LeetCode/Solution.java
@@ -0,0 +1,7 @@
+package LeetCode;
+
+class Solution {
+    public int fib(int n) {
+        return -1;
+    }
+}
--- src/main/java/tools/KPM.java
+++ src/main/java/tools/KPM.java
@@ -1,6 +1,6 @@
-package org.LeetCode;
+package tools;
 
-public class Tools {
+public class KPM {
     /**
      * 使用KMP算法查找模式字符串在匹配字符串中的位置
      *
@@ -8,7 +8,7 @@ public class Tools {
      * @param patternStr 模式字符串，即我们要在匹配字符串中查找的字符串
      * @return 如果找到模式字符串，则返回其在匹配字符串中首次出现的位置；如果未找到，则返回-1
      */
-    public int KPM(char[] matchedStr, char[] patternStr) {
+    public int kpm(char[] matchedStr, char[] patternStr) {
         int[] next = new int[patternStr.length];
         int j = 0;
         for (int i = 1; i < patternStr.length; i++) {
@@ -17,7 +17,7 @@ public class Tools {
             next[i] = j;
         }
         j = 0;
-        for (int i = 0; i < patternStr.length; i++) {
+        for (int i = 0; i < matchedStr.length; i++) {
             while (j > 0 && matchedStr[i] != patternStr[j]) j = next[j - 1];
             if (matchedStr[i] == patternStr[j]) j++;
             if (j == patternStr.length) return i - patternStr.length + 1;
